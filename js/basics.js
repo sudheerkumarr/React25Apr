671399b47d3a5ae533a80642feb511cf267396c1
Sudheer <rskumar780@gmail.com>
2022-04-28 12:46:06 +0530

    added es6

--- js/basics.js
+++ js/basics.js
@@ -172,3 +172,139 @@ greet();
 const isEven = (num) => (num % 2 == 0 ? true : false);
 
 console.log(isEven(10)); // true
+
+//function with default parameters
+function mul(a, b = 10) {
+  return a * b;
+}
+console.log(mul(10, 20)); // 200
+console.log(mul(10)); // 100
+
+// Rest parameters (...)
+/*
+ function add(x, y) {
+     return x+y;
+ }
+ function add(x, y, z) {
+     return x+y+z;
+ }
+*/
+
+function add(x, ...nums) {
+  let sum = x;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum;
+}
+
+console.log(add(10)); // 10
+console.log(add(10, 20)); // 30
+console.log(add(10, 20, 30)); // 60
+console.log(add(10, 20, 30, 40, 50)); // 150
+
+// Spread Operator(...) - copy an array - es6
+const shapes = ["triangle", "square"];
+const newShapes = [...shapes];
+console.log();
+console.log(shapes);
+console.log(newShapes);
+
+const newShapes1 = ["circle", ...shapes];
+console.log(newShapes1);
+const newShapes2 = [...shapes, "cube"];
+console.log(newShapes2);
+const newShapes3 = ["circle", ...shapes, "cube"];
+console.log(newShapes3);
+
+// Destructuring - es6
+// 1. object destructuring
+// 2. array destructuring
+/*
+const person = {
+  name: "Raj",
+  age: 20,
+  contactNos: [9999912345, 9999912346],
+  address: [
+    {
+      city: "Chennai",
+      state: "Tamilnadu",
+    },
+    {
+      city: "Bangalore",
+      state: "Karnataka",
+    },
+  ],
+};
+
+*/
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.contactNos[0]);
+console.log(person.address[0].city);
+
+// Object destructuring
+const { name, age } = person;
+console.log();
+console.log(name);
+console.log(age);
+
+// Array Destructuring
+const [num1, num2] = person.contactNos;
+// person.contactNos[0]
+// person.contactNos[1]
+
+console.log(num1);
+console.log(num2);
+
+const [addr1, addr2] = person.address;
+// person.address[0];
+// person.address[1];
+
+console.log(addr1);
+console.log(addr2);
+
+// map & filter - es6
+// map - applies same functionality on each and every element present in array
+const numArr = [10, 20, 30, 40];
+
+const sq = numArr.map((n) => n * n);
+console.log(sq);
+
+// function square(num) {
+//     return num*num;
+// }
+// (num) =>  num*num;
+
+// filter - filter based on condition
+const result = numArr.filter((n) => n > 20);
+console.log(result);
+const evnNums = numArr.filter((n) => n % 2 == 0);
+console.log(evnNums);
+
+// class - es6
+class Employee {
+  constructor(empId, empName) {
+    this.empId = empId;
+    this.empName = empName;
+  }
+  setEmpId(empId) {
+    this.empId = empId;
+  }
+  getEmpId() {
+    return this.empId;
+  }
+}
+const emp1 = new Employee(1001, "Raj");
+const emp2 = new Employee(1002, "Sam");
+console.log(emp1);
+console.log(emp2);
+
+console.log(emp1.getEmpId()); // 1001
+console.log(emp2.getEmpId()); // 1002
+emp2.setEmpId(1005);
+console.log(emp2.getEmpId()); // 1005
+
+
+// export & import
